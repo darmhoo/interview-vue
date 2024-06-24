535abb8158e248611b320b6b2898bc9a5c49147b
Damola Oladeji <darmhoo@hotmail.com>
2024-06-24 14:58:02 +0100

    fixes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,26 +4,45 @@ export default {
   data() {
     return {
       key: "a6e5ec0565d247b48e9114706242406",
-      weatherData: null,
+      currentWeatherData: null,
+      foreCast: null,
+
       baseApi: "http://api.weatherapi.com/v1"
     }
   },
   methods: {
     async getWeatherData() {
-      let res = await axios.get(`${this.baseApi}/forecase.json?key=${this.key}`);
-      console.log(res);
+      let res = await axios.get(`${this.baseApi}/forecast.json?key=${this.key}&q=Lagos`);
+      this.currentWeatherData = res.data;
+    },
+
+    async getWeatherForecast() {
+      let res = await axios.get(`${this.baseApi}/forecast.json?key=${this.key}&q=Lagos&days=14`);
+      this.foreCast = res.data.forecast;
     }
   },
 
   mounted(){
     this.getWeatherData();
+    this.getWeatherForecast();
   }
 }
 
 </script>
 
 <template>
-  <div class="greetings">
+  <div class="">
+   <h2>The Current Weather Data for Lagos Today</h2>
+   <div>
+    <p>
+      <span>Cloud: </span> <span> {{ currentWeatherData?.current?.cloud }}</span><br>
+      <span>Condition: </span> <span> {{ currentWeatherData?.current?.condition.text }}</span> <span> <img :src="currentWeatherData?.current?.condition.icon" alt=""></span><br>
+      <span>Dew Point: </span> <span> {{ currentWeatherData?.current?.dewpoint_c }}</span><br>
+      <span>Pressure: </span> <span> {{ currentWeatherData?.current?.pressure_in }}</span><br>
+
+    </p>
+   </div>
+
 
   </div>
 </template>
